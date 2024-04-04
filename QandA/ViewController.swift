b42f9c16dfb8e5657a17dce2d1106e919e4b2b78
kai <kai810215@gmail.com>
2024-04-04 09:49:11 +0800

    Initial Commit

---
+++ QandA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QandA
+//
+//  Created by Ryan on 2024/4/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
